d29dedeb64e7fa35fe01b50d9cb61e0eac97905b
gabrieuu <mcgabriel208@gmail.com>
2023-05-17 11:46:50 -0300

    finalizado CSS e HTML. Dando Inicio a o Javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-//d0d90eeb88c6eebb728e9d94a353ae06
+/// flat/64.png
+
+//variaveis
+const apiKey = "d0d90eeb88c6eebb728e9d94a353ae06";
+const apiCountryURL = "https://flagsapi.com/"
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+
+//funcoes
+const showWeatherData = (city) =>{
+    console.log(city);
+}
+//eventos
+
+searchBtn.addEventListener("click",(e) =>{
+    e.preventDefault();
+    const city = cityInput.value;
+    showWeatherData(city);
+})
