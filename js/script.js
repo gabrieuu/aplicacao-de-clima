13a696dd9441406b9b6ef60ac0bf92f289929a52
gabrieuu <mcgabriel208@gmail.com>
2023-05-17 14:35:58 -0300

    finalizando projeto

--- js/script.js
+++ js/script.js
@@ -7,10 +7,33 @@ const apiCountryURL = "https://flagsapi.com/"
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
 
+const informacoes = document.querySelector("#weather-data");
+const cityElement = document.querySelector("#city");
+const bandeira = document.querySelector("#country");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
 
 //funcoes
-const showWeatherData = (city) =>{
-    console.log(city);
+const getWeatherData = async (city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+    return data;
+}   
+const showWeatherData = async(city) =>{
+    const data = await getWeatherData(city); 
+    
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute("src",`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+    umidityElement.innerText = data.main.humidity+"%"
+    windElement.innerText = data.wind.speed+"km/h";
+    bandeira.setAttribute("src",apiCountryURL + data.sys.country+"/flat/64.png");
+    informacoes.classList.remove("hide");
 }
 //eventos
 
@@ -18,4 +41,11 @@ searchBtn.addEventListener("click",(e) =>{
     e.preventDefault();
     const city = cityInput.value;
     showWeatherData(city);
+    
+})
+cityInput.addEventListener("keyup", (e) =>{
+    if(e.code === "Enter"){
+        const city = e.target.value;
+        showWeatherData(city);
+    }
 })
